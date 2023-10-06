2a2bfa23abd48aee054c74aa9069a76bd883c36a
Grace <graceadegunle16@gmail.com>
2023-10-06 17:38:26 +0100

    first commit

---
+++ src/component/LoginPage/ProtectedRouters.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import Join from "../Join/Join";
+const useAuth = () => {
+  const user = { loggedIn: false };
+  return user && user.loggedIn;
+};
+
+const ProtectedRouters = () => {
+  const isAuth = useAuth();
+
+  return isAuth ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default ProtectedRouters;
